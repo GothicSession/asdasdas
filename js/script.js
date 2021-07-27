46231b44223e2a588d19d9c0a3be68adb011fa94
Artem <artembildaev12@gmail.com>
2021-07-27 11:48:51 +0300

    Slider Added

--- js/script.js
+++ js/script.js
@@ -264,4 +264,53 @@ window.addEventListener('DOMContentLoaded', () => {
             },4000);
         }
 
+        //Slider
+        const slides=document.querySelectorAll('.offer__slide'),
+        previous=document.querySelector('.offer__slider-prev'),
+        next=document.querySelector('.offer__slider-next'),
+        total=document.querySelector('#total'),
+        current=document.querySelector('#current');
+        let slideIndex=1;
+
+        showSlides(slideIndex);
+        
+        if (slides.length<10){
+            total.textContent=`0${slides.length}`;
+        }else{
+            total.textContent=slides.length;
+        }
+
+
+        function showSlides(n){
+            if(n>slides.length){
+                slideIndex=1;
+            }
+
+            if (n<1){
+                slideIndex=slides.length;
+            }
+
+            slides.forEach(item=>item.style.display='none');
+
+            slides[slideIndex-1].style.display='block';
+
+            if (slides.length<10){
+                current.textContent=`0${slideIndex}`;
+            }else{
+                current.textContent=slideIndex;
+            }
+        }
+
+        function plusSlides(n){
+            showSlides(slideIndex += n);
+        }
+
+        previous.addEventListener('click',()=>{
+            plusSlides(-1);
+        });
+
+        next.addEventListener('click',()=>{
+            plusSlides(1);
+        });
+
 });
